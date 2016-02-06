75e2f536cccc3d8524574258dd45dccb7e53870b
zeljkokunica <zeljko.kunica@gmail.com>
2016-02-06 21:51:48 +0100

    test

---
+++ src/main/java/hr/cleancode/MyApplication.java
@@ -0,0 +1,11 @@
+package hr.cleancode;
+
+/**
+ * Created by zac on 06/02/16.
+ */
+public class MyApplication {
+
+    public static void main(String[] args) {
+        System.out.println("ŽžČčĆćŠšĐđ");
+    }
+}
